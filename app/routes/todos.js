5104f0a20a1468275877d8002390b1834cc452eb
Peterortiz82 <peterortiz82@gmail.com>
2015-07-28 22:33:49 -0400

    add ability to edit and delete a todo

--- app/routes/todos.js
+++ app/routes/todos.js
@@ -5,19 +5,26 @@ export default Ember.Route.extend({
     return this.store.find('todo');
   },
   actions: {
-    createTodo: function(newTitle) {
-        // Create the new Todo model
-        var todo = this.store.createRecord('todo', {
-          title: newTitle,
-          isCompleted: false
-        });
-        
-        // Clear the "New Todo" text field
-        this.controllerFor('todos').set('newTitle', '');
+    createTodo: function (newTitle) {
+      // Create the new Todo model
+      var todo = this.store.createRecord('todo', {
+        title: newTitle,
+        isCompleted: false
+      });
 
-        // Save the new model
+      // Save the new model
+      todo.save();
+    },
+    acceptChanges: function(todo) {
+      if (Ember.isEmpty(todo.get('title'))) {
+        this.send('deleteTodo', todo);
+      } else {
         todo.save();
       }
+    },
+    deleteTodo: function(todo) {
+      todo.deleteRecord();
     }
-  });
+  }
+});
 
